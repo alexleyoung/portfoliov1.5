493c0781086f49d14986c71638c41646d2882a87
alx-yng <alexyoung0119@gmail.com>
2024-08-06 19:44:34 -0500

    dev: update leader cards

--- components/sections/leadership/grid.tsx
+++ components/sections/leadership/grid.tsx
@@ -193,91 +193,70 @@ export const CloseIcon = () => {
 
 const cards = [
   {
-    description: "Lana Del Rey",
-    title: "Summertime Sadness",
-    src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
+    title: "President",
+    description: "CSE Club @ ISU",
+    src: "/images/leadership/cselogo.png",
     ctaText: "Visit",
     ctaLink: "https://ui.aceternity.com/templates",
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
+          Lead a team of 6 officers to plan and execute 20+ events per semester.
+          Increased club membership by 30% and garnered ~$10,000 in funding from
+          sponsors.
         </p>
       );
     },
   },
   {
-    description: "Babbu Maan",
-    title: "Mitran Di Chhatri",
-    src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
+    title: "Peer Mentor",
+    description: "CS Department @ ISU",
+    src: "/images/leadership/isucslogo.jpg",
     ctaText: "Visit",
     ctaLink: "https://ui.aceternity.com/templates",
     content: () => {
       return (
         <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-          voice and profound lyrics that resonate deeply with his audience. Born
-          in the village of Khant Maanpur in Punjab, India, he has become a
-          cultural icon in the Punjabi music industry. <br /> <br /> His songs
-          often reflect the struggles and triumphs of everyday life, capturing
-          the essence of Punjabi culture and traditions. With a career spanning
-          over two decades, Babu Maan has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+          Mentored 25 first-year CS students on CS/SWE pathways at ISU. Led a
+          half-semester class focused on facilitating socialization and
+          awareness CS opportunities. Hosted weekly office hours and study
+          sessions. Helped students with homework, projects, and exam prep.
         </p>
       );
     },
   },
 
   {
-    description: "Metallica",
-    title: "For Whom The Bell Tolls",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    title: "Peer Educator",
+    description: "Multicultural Student Affairs @ ISU",
+    src: "/images/leadership/msalogo.png",
     ctaText: "Visit",
     ctaLink: "https://ui.aceternity.com/templates",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          Helped 20+ high-achieving multicultural students navigate their first
+          semester at ISU. Assisted MSA in hosting 7 events throughout each
+          semester. Answer student questions about work/life balance, goal
+          setting, future planning, etc.
         </p>
       );
     },
   },
   {
-    description: "Lord Himesh",
-    title: "Aap Ka Suroor",
-    src: "https://assets.aceternity.com/demos/aap-ka-suroor.jpeg",
+    title: "First-Year Honors Leader",
+    description: "ISU Honors",
+    src: "/images/leadership/isuhonors.png",
     ctaText: "Visit",
     ctaLink: "https://ui.aceternity.com/templates",
     content: () => {
       return (
         <p>
-          Himesh Reshammiya, a renowned Indian music composer, singer, and
-          actor, is celebrated for his distinctive voice and innovative
-          compositions. Born in Mumbai, India, he has become a prominent figure
-          in the Bollywood music industry. <br /> <br /> His songs often feature
-          a blend of contemporary and traditional Indian music, capturing the
-          essence of modern Bollywood soundtracks. With a career spanning over
-          two decades, Himesh Reshammiya has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+          Led 12 first-year ISU Honors students through their first semester.
+          Co-led a semester-long, weekly seminar with 15 unique lesson plans
+          focused on allowing the students to thrive academically and personally
+          at ISU. Helped students with course planning and navigating university
+          struggles.
         </p>
       );
     },
