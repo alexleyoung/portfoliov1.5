16511a497e94e4368837eaf74be6c290ee4a9bd1
alx-yng <alexyoung0119@gmail.com>
2024-08-06 19:56:07 -0500

    fix: add transition for leader card hover

--- components/sections/leadership/grid.tsx
+++ components/sections/leadership/grid.tsx
@@ -132,7 +132,7 @@ export function Grid() {
             layoutId={`card-${card.title}-${id}`}
             key={card.title}
             onClick={() => setActive(card)}
-            className='p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer'>
+            className='p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer transition-colors'>
             <div className='flex gap-4 flex-col  w-full'>
               <motion.div layoutId={`image-${card.title}-${id}`}>
                 <Image
